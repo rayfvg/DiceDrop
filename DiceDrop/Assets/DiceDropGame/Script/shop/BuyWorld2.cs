f581b236fcb8c8c62c573e1716132f9cb7b61f55
rayfvg <rayfvg@yandex.ru>
2024-08-06 21:01:19 +0300

    up

---
+++ DiceDrop/Assets/DiceDropGame/Script/shop/BuyWorld2.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyWorld2 : MonoBehaviour
+{
+    public Button ButtonPLayWorld2;
+    public GameObject ClosedWorld2;
+
+    public TMP_Text textMoneyCount;
+
+    public int PriceForBuying = 10;
+    private int _money;
+
+    private void Start()
+    {
+        _money = PlayerPrefs.GetInt("money");
+
+        if (PlayerPrefs.GetInt("World2") == 1)
+        {
+            ClosedWorld2.SetActive(false);
+            ButtonPLayWorld2.interactable = true;
+        }
+    }
+    public void TryBuyWorld2()
+    {
+        if(_money >= PriceForBuying)
+        {
+            _money -= PriceForBuying;
+            PlayerPrefs.SetInt("money", _money);
+            textMoneyCount.text = _money.ToString();
+            ClosedWorld2.SetActive(false);
+            ButtonPLayWorld2.interactable = true;
+            PlayerPrefs.SetInt("World2", 1);
+        }
+    }
+}
---
+++ DiceDrop/Assets/DiceDropGame/Script/shop/TestScript.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private int _money;
+    public TMP_Text textMoney;
+    public Wallet wallet;
+
+    private void Start()
+    {
+        _money = PlayerPrefs.GetInt("money");
+    }
+    public void ResetAllSave()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void AddMyMany()
+    {
+        textMoney.text = PlayerPrefs.GetInt("money").ToString();
+        wallet.AddMoney(2);
+    }
+}
